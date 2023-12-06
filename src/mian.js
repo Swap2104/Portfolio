4656db5d1c9bcc41180a1784a1ebdad99f72b65a
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2023-12-06 13:25:05 +0530

    hover animaition to home text

--- src/mian.js
+++ src/mian.js
@@ -1,6 +1,9 @@
 const body = document.getElementsByTagName('body')
 const sections = document.querySelectorAll('section')
-const langs=document.querySelectorAll(".right, .left ,.about-me ,.github-card")
+const langs = document.querySelectorAll(".right, .left ,.about-me ,.github-card")
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let interval = null;
+
 
 //? intersection observer-- used to change background color on scroll
 const observer = new IntersectionObserver(e => {
@@ -13,7 +16,6 @@ const observer = new IntersectionObserver(e => {
     }
     e.forEach(el => {
         if (el.isIntersecting) {
-            console.log(el.target)
             body[0].style.backgroundColor = color[el.target.id]
         } else {
 
@@ -21,27 +23,64 @@ const observer = new IntersectionObserver(e => {
     })
 })
 
-sections.forEach(e => {
-    observer.observe(e)
-})
+sections.forEach(e => observer.observe(e))
 
-console.log(langs)
 // ? intersection observer-- used for on scroll animation
-const animation=new IntersectionObserver(e=>{
-    e.forEach(el=>{
-        if(el.isIntersecting){
+const animation = new IntersectionObserver(e => {
+    e.forEach(el => {
+        if (el.isIntersecting) {
             console.log(el.target)
             el.target.classList.remove("hide")
             el.target.classList.add("show")
-        }else{
+        } else {
             el.target.classList.remove("show")
             el.target.classList.add("hide")
         }
     })
 })
 
-
-langs.forEach(e=>{
+langs.forEach(e => {
     e.classList.add("hide")
     animation.observe(e)
 })
+
+const home_animation = new IntersectionObserver(e => {
+    e.forEach(el => {
+        if (el.isIntersecting) {
+            el.target.classList.add("h1-animation")
+        } else {
+            el.target.classList.remove("h1-animation")
+        }
+    })
+})
+
+document.querySelectorAll(".hello").forEach(e => home_animation.observe(e))
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector(".h1-animation").onmouseover = event => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+            .split("")
+            .map((letter, index) => {
+                if (index < iteration) {
+                    return event.target.dataset.value[index];
+                }
+
+                return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+
+        if (iteration >= event.target.dataset.value.length) {
+            clearInterval(interval);
+        }
+
+        iteration += 1 / 3;
+    }, 40);
+}
